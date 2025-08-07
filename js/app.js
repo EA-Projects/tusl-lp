eb36e64089b5cdb03a928e0a05aea6fbb7e69429
Joel Bidal <joelbidal1@hotmail.com>
2025-08-07 12:54:03 -0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,109 @@
+window.addEventListener('load', function() {   
+    // Firebase configuration
+    const firebaseConfig = {
+        apiKey: "AIzaSyC_Q6NGnDyKd8WM7e4yxg757_yB-g7QsRo",
+        authDomain: "tusl-5a507.firebaseapp.com",
+        projectId: "tusl-5a507",
+        appId: "1:775259817894:web:a3cc3e8dcdb656619b7167",
+    };
+
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    const db = firebase.firestore();
+
+    // Main form to send data to Firestore [Email and Platform]
+    if ($('#waitlist-form').length) {
+        document.getElementById("waitlist-form").addEventListener("submit", async function (e) {
+            e.preventDefault();
+
+            const email = document.getElementById("email").value;
+            const platform = document.querySelector('input[name="platform"]:checked').value;
+
+            try {
+                const docRef = await db.collection('waitlist').add({
+                    email: email,
+                    platform: platform,
+                    timestamp: firebase.firestore.FieldValue.serverTimestamp()
+                });
+
+                // Save the ID in localStorage to use it later in success.html
+                localStorage.setItem('waitlistDocId', docRef.id);
+
+                // Redirect to success page
+                window.location.href = "/success.html";
+            } catch (error) {
+                console.error("Error registering in Firestore:", error);
+            }
+        });
+    }
+
+    // Referral form to update the existing document with referred emails
+    if ($('#referral-form').length) {
+        document.getElementById("referral-form").addEventListener("submit", async function (e) {
+            e.preventDefault();
+
+            // Obtener el ID guardado
+            const docId = localStorage.getItem('waitlistDocId');
+
+            if (!docId) {
+                // Warning if no ID is found
+                // alert("No se encontró información previa. Vuelve a la página anterior.");
+                return;
+            }
+
+            // Get referred emails (filter out empty ones)
+            const referrals = Array.from(document.querySelectorAll(".referral-input"))
+                .map(input => input.value.trim())
+                .filter(email => email.length > 0);
+
+            try {
+                // Update the existing document with the referrals
+                await db.collection("waitlist").doc(docId).update({
+                    referrals: referrals
+                });
+
+                // You can show a success message or redirect the user
+                $('.success-form').fadeIn();
+            } catch (error) {
+                console.error("Error saving referrals:", error);
+            }
+        });
+    }
+
+
+    // Animations
+    let animatedElements = new Set(); // Para evitar reanimaciones
+
+    gsap.set("[data-fade]", { opacity: 0, y: 20 });
+
+    let observer = new IntersectionObserver((entries) => {
+        // Filtrar los elementos que están entrando en vista y no han sido animados aún
+        const toAnimate = entries
+            .filter(entry => entry.isIntersecting && !animatedElements.has(entry.target))
+            .map(entry => entry.target);
+
+        if (toAnimate.length > 0) {
+            gsap.to(toAnimate, {
+                opacity: 1,
+                y: 0,
+                stagger: 0.15,
+                ease: "power2.out",
+                duration: 0.5,
+                delay: 0.3
+            });
+
+            // Marcar los elementos como animados y dejar de observarlos
+            toAnimate.forEach(el => {
+                animatedElements.add(el);
+                observer.unobserve(el);
+            });
+        }
+    }, {
+        threshold: 0.3
+    });
+
+    document.querySelectorAll("[data-fade]").forEach((el) => {
+        observer.observe(el);
+    });
+
+});
